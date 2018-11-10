675d949712cfd5efaa21d95c077c4c0d2d89abda
playerkeisukefall <playerkeisukefall@gmail.com>
2018-11-11 05:11:39 +0900

    add: feedout animation

--- q_num_select/q_num_select.js
+++ q_num_select/q_num_select.js
@@ -3,12 +3,24 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
     $("#btn10").click(function(){
-      location.href = "../main/main.html?num=10";
+      setTimeout(() => {
+        location.href = "../main/main.html?num=10";
+      }, 1000);
+      $(".btn2").css({'animation': 'feedout 3s'});
+      $(".btn3").css({'animation': 'feedout 3s'});
     })
     $("#btn20").click(function(){
-      location.href = "../main/main.html?num=20";
+      setTimeout(() => {
+        location.href = "../main/main.html?num=20";
+      }, 1000);
+      $(".btn1").css({'animation': 'feedout 3s'});
+      $(".btn3").css({'animation': 'feedout 3s'});
     })
     $("#btn30").click(function(){
-      location.href = "../main/main.html?num=30";
+      setTimeout(() => {
+        location.href = "../main/main.html?num=30";
+      }, 1000);
+      $(".btn1").css({'animation': 'feedout 3s'});
+      $(".btn2").css({'animation': 'feedout 3s'});
     })
   });
